fc992edd06056e669b4ae97eec5be7cba2735aa1
mariia <mkhavantseva@gmail.com>
2023-04-28 18:12:46 +0400

    создала константы

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+private let AccessKey = "zFXRehhM6qvSJar3CUO1PxqqBpIhfGTsgKPIqrRqBzs"
+private let SecretKey = "k9R3ecEXTerZwHH0XOcE9aMASq8cK9beJ0o4EXlgAzA"
+private let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+private let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
